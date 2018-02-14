34e6cdb8d1c76135aa976b29ce8139037b4c97bb
Rajat <mangal.rajat98@gmail.com>
2018-02-14 05:55:25 -0500

    edit article working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,15 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  
   def create
-#     render plain: params[:article].inspect
     @article = Article.new(article_params)
-#     @article.save
     if @article.save
       flash[:notice] = "Articles was successfully created"
       redirect_to article_path(@article)
@@ -13,11 +18,26 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+  
+  
   def show
     @article = Article.find(params[:id])
   end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+    
+    
   private
   def article_params
     params.require(:article).permit(:title, :description)
   end
+ 
 end
